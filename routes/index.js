7528e24dbf9b0742ecdd10eb3565fcabf7528896
郭轩 <guoxuan@guoxuan.local>
2017-05-15 12:06:12 +0800

    OK

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const Jssdk = require('../services/jssdk.js');
+
+router.use(Jssdk.jssdk);
 
 /* GET home page. */
 router.get('/buy', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    jssdk: req.jssdk
+  });
 });
 
 module.exports = router;
