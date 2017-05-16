922e02601e34c2e03f6ae80ec630f3e2c5ecdaee
lvbiao <lvb_8231853@qq.com>
2017-05-16 14:58:28 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ const oAuth = require('../services/oAuth.js');
 router.use(Jssdk.jssdk);
 
 /* GET home page. */
-router.get('/', oAuth.oAuth, function(req, res, next) {
+// router.get('/', oAuth.oAuth, function(req, res, next) {
+router.get('/', function(req, res, next) {
   //获取index页面的走马灯图片
   var indexSvc = new IndexSvc();
   Promise.all([indexSvc.getSwipers(), indexSvc.getTodayItems()]).then(data => {
