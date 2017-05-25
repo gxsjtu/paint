0027ea87ddc5c16e3e136bd7c735c74c93287ac2
崔青杰 <guoxuan@kakuxuandeMac-mini.local>
2017-05-25 08:56:54 +0800

    update

--- routes/myGallery.js
+++ routes/myGallery.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const Jssdk = require('../services/jssdk.js');
 const oAuth = require('../services/oAuth.js');
 const ItemSvc = require('../services/itemSvc.js');
+const Errors = require('../services/error.js');
 var itemSvc = new ItemSvc();
 
 router.use(Jssdk.jssdk);
