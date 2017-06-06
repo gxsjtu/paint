7f1a381eb7d8746df0c3171b9e4a305f282a3d09
lvbiao <lvb_8231853@qq.com>
2017-06-06 09:46:59 +0800

    update

--- routes/wxpay.js
+++ routes/wxpay.js
@@ -5,20 +5,25 @@ const Promise = require('promise');
 const Result = require('../services/result.js');
 const Errors = require('../services/error.js');
 const oAuth = require('../services/oAuth.js');
+const _ = require('lodash');
 const ItemSvc = require('../services/itemSvc.js');
 var itemSvc = new ItemSvc();
 
-router.get('/pay/:itemId', oAuth.oAuth, function(req, res, next) {
+router.get('/pay/:itemId', function(req, res, next) {
+  //res.render("wxpay");
   var itemId = req.params.itemId;
-  itemSvc.getItemById(itemId).then(data => {
-    if(data){
+  itemSvc.getItemById(itemId).then(item => {
+    if(item){
       var paySvc = new PaySvc();
-      var price = data.price * 100;
+      var lastBids = _.max(item.bids, 'price');
+      var price = lastBids.price * 100;
       var openId = req.session.openId;
       var payargs = '';
       paySvc.getBrandWCPayRequestParams(openId, itemId, price).then(data => {
         res.render("wxpay", {
-          payargs: data
+          payargs: data,
+          item:item,
+          price:price
         });
       }).catch(err => {
         res.status(404).end();
@@ -27,9 +32,7 @@ router.get('/pay/:itemId', oAuth.oAuth, function(req, res, next) {
     else{
       res.json(new Result(Errors.GetItemsFailed));
     }
-
   }).catch(err => res.json(new Result(Errors.GetItemsFailed, err)));
-
 });
 
 router.get('/notify', oAuth.oAuth, function(req, res, next) {
